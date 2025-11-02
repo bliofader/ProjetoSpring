fbe0dfb30b487f5be6414b5d50157ee36539799a
Icaro da Silveira <icaro.sila@gmail.com>
2025-11-02 00:40:29 -0300

    Enviando classes frontend, páginas de cadastro de usuário, exercícios, listar usuários e listar exercícios. Também criados novos componentes e rotas de navegação.

--- src/app/pages/home/admin/admin.component.ts
+++ src/app/pages/home/admin/admin.component.ts
@@ -18,7 +18,7 @@ import { HeaderTopComponent } from '../../../components/headertop/headertop.comp
 export class AdminCadastroUsuarioComponent {
   searchTerm = '';
   usuarios = [
-    { ID: '1', Nome: 'Icaro', Tipo: 'Usuario', DataDeNascimento: '06/06/1995', CPF: '448.875.485-77', Email: 'icaro.123@gmail.com', Senha: 'icaro123', ativo: true },
+    { ID: '1', Nome: 'Icaro', Tipo: 'Usuario', DataDeNascimento: '06/06/1993', CPF: '448.875.485-77', Email: 'icaro.123@gmail.com', Senha: 'icaro123', ativo: true },
     { ID: '2', Nome: 'Maria', Tipo: 'Personal', DataDeNascimento: '12/12/2005', CPF: '265.875.485-76', Email: 'maria.123@gmail.com', Senha: 'maria123', inativo: true },
     { ID: '3', Nome: 'Ellen', Tipo: 'Personal', DataDeNascimento: '13/04/2003', CPF: '328.875.665-54', Email: 'ellen.123@gmail.com', Senha: 'ellen123', ativo: true },
   ];
