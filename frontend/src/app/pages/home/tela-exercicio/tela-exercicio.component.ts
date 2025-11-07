6f083d096456b3eb11ed3a74fd1b3f13419b9ad6
KAUA DIODATO DA SILVA <kaua.dsilva3@senacsp.edu.br>
2025-11-07 12:06:21 -0300

    enviando alterações com envio da imagem

--- frontend/src/app/pages/home/tela-exercicio/tela-exercicio.component.ts
+++ frontend/src/app/pages/home/tela-exercicio/tela-exercicio.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from "../../../components/header/header.component";
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from "../../../components/footer/footer.component";
 import { RouterModule } from '@angular/router';
+import { ExercicioService } from '../../../services/exercicio.service';
 import { Exercicio } from '../../../entities/exercicio';
 
 @Component({
@@ -14,40 +15,31 @@ import { Exercicio } from '../../../entities/exercicio';
 })
 export class TelaExercicioComponent implements OnInit {
   selectedCategory: string = 'all';
+  exercises: Exercicio[] = [];
+  filteredExercises: Exercicio[] = [];
 
-  //banco temporario
-  exercises: Exercicio[] = [
-    { nome: 'Agachamento Livre', agrupamento: 'pernas', imagePath: 'assets/images/pernas.png', tipo: 'força', descricao: 'Exercício básico para o desenvolvimento dos músculos das pernas e glúteos.', nivel: 'básico', videoUrl: 'https://www.youtube.com/embed/86ZW7tmmLuU?si=H6zrJ3uIFWE0d6YQ' },
-    { nome: 'Leg Press', agrupamento: 'quadriceps', imagePath: 'assets/images/pernas.png', tipo: 'força', descricao: 'Exercício básico para o desenvolvimento dos músculos das pernas e glúteos.', nivel: 'intermediário', videoUrl: 'https://www.youtube.com/embed/86ZW7tmmLuU?si=H6zrJ3uIFWE0d6YQ' },
-    { nome: 'Panturrilha em Pé', agrupamento: 'panturrilha', imagePath: 'assets/images/pernas.png', tipo: 'resistência', descricao: 'Exercício básico para o desenvolvimento dos músculos das pernas e glúteos.', nivel: 'básico', videoUrl: 'https://www.youtube.com/embed/86ZW7tmmLuU?si=H6zrJ3uIFWE0d6YQ' },
-    { nome: 'Supino Reto', agrupamento: 'peito', imagePath: 'assets/images/pernas.png', tipo: 'força', descricao: 'Exercício básico para o desenvolvimento dos músculos do peitoral e tríceps.', nivel: 'intermediário', videoUrl: 'https://www.youtube.com/embed/86ZW7tmmLuU?si=H6zrJ3uIFWE0d6YQ' },
-    { nome: 'Remada Curvada', agrupamento: 'costas', imagePath: 'assets/images/pernas.png', tipo: 'força', descricao: 'Exercício básico para o fortalecimento dos músculos das costas e bíceps.', nivel: 'avançado', videoUrl: 'https://www.youtube.com/embed/86ZW7tmmLuU?si=H6zrJ3uIFWE0d6YQ' },
-    { nome: 'Elevação Lateral', agrupamento: 'ombro', imagePath: 'assets/images/pernas.png', tipo: 'isolado', descricao: 'Exercício básico para o desenvolvimento dos músculos laterais dos ombros.', nivel: 'intermediário', videoUrl: 'https://www.youtube.com/embed/86ZW7tmmLuU?si=H6zrJ3uIFWE0d6YQ' },
-    { nome: 'Prancha Abdominal', agrupamento: 'abs', imagePath: 'assets/images/pernas.png', tipo: 'resistência', descricao: 'Exercício básico para o fortalecimento do abdômen e estabilidade corporal.', nivel: 'básico', videoUrl: 'https://www.youtube.com/embed/86ZW7tmmLuU?si=H6zrJ3uIFWE0d6YQ' },
-    { nome: 'Tríceps Corda', agrupamento: 'triceps', imagePath: 'assets/images/pernas.png', tipo: 'isolado', descricao: 'Exercício básico para o fortalecimento e definição do tríceps.', nivel: 'intermediário', videoUrl: 'https://www.youtube.com/embed/86ZW7tmmLuU?si=H6zrJ3uIFWE0d6YQ' },
-    { nome: 'Rosca Direta', agrupamento: 'biceps', imagePath: 'assets/images/pernas.png', tipo: 'isolado', descricao: 'Exercício básico para o desenvolvimento dos músculos do bíceps.', nivel: 'básico', videoUrl: 'https://www.youtube.com/embed/86ZW7tmmLuU?si=H6zrJ3uIFWE0d6YQ' },
-    { nome: 'Rosca Alternada', agrupamento: 'biceps', imagePath: 'assets/images/pernas.png', tipo: 'isolado', descricao: 'Exercício básico para o fortalecimento e simetria dos músculos do bíceps.', nivel: 'intermediário', videoUrl: 'https://www.youtube.com/embed/86ZW7tmmLuU?si=H6zrJ3uIFWE0d6YQ' }
+  constructor(private exercicioService: ExercicioService) {}
 
-  ];
+  ngOnInit(): void {
+    this.exercicioService.listarExercicios().subscribe({
+      next: (res) => {
+        this.exercises = res;
+        this.filteredExercises = res;
+      },
+      error: (err) => {
+        console.error('Erro ao carregar exercícios', err);
+      }
+    });
+  }
 
-
-
-  filteredExercises = [...this.exercises];
-
-  ngOnInit() { }
-
-  filterExercises(category: string) {
+  filterExercises(category: string): void {
     this.selectedCategory = category;
     if (category === 'all') {
       this.filteredExercises = this.exercises;
     } else {
       this.filteredExercises = this.exercises.filter(
-        (exercicio) => exercicio.agrupamento === category
+        (exercicio) => exercicio.agrupamento.toLowerCase() === category.toLowerCase()
       );
     }
-  }
-
-  findAll(): void { //nome do metodo(findAll) e tipo de retorno (void)
-
   }
 }
